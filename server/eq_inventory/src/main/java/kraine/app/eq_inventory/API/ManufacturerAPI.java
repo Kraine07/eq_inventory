6625d32b24d628952d1442c3068087a1a1c46942
Kraine07 <aundre.bushay@gmail.com>
2025-09-29 14:44:00 -0500

    fixed delete manufacturer api

--- server/eq_inventory/src/main/java/kraine/app/eq_inventory/API/ManufacturerAPI.java
+++ server/eq_inventory/src/main/java/kraine/app/eq_inventory/API/ManufacturerAPI.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -60,7 +61,7 @@ class ManufacturerAPI {
 
 
     @PostMapping("/delete-manufacturer/{id}")
-    public ResponseEntity<?> deleteManufacturer(@RequestParam Long id) {
+    public ResponseEntity<?> deleteManufacturer(@PathVariable Long id) {
 
         try {
             manufacturerService.deleteManufacturer(id);
--- server/eq_inventory/src/main/java/kraine/app/eq_inventory/service/ManufacturerService.java
+++ server/eq_inventory/src/main/java/kraine/app/eq_inventory/service/ManufacturerService.java
@@ -4,8 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheConfig;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import jakarta.transaction.Transactional;
